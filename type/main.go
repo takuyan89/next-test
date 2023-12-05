3641e81ffe7996271ade54d9b917491ce536f3c1
Takuya <git.takuya89@gmail.com>
2023-12-05 21:54:44 +0100

    🎨

---
+++ type/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var i int = 100
+
+	// var i2 int64 = 200
+
+	// fmt.Println(i + 50)
+
+	// fmt.Printf("%T\n", i2)
+
+	// fmt.Printf("%T\n", int32(i2))
+
+	// fmt.Println(i + int(i2))
+
+
+
+	var fl64 float64 = 2.4
+	fmt.Println(fl64)
+
+	fl := 3.2
+	fmt.Printf("%T, %T\n", fl64, fl)
+
+	var fl32 float32 = 1.2
+	fmt.Printf("%T\n", fl32)
+
+}
