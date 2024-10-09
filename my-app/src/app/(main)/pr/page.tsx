5638c4c6b79877b2c56f4e3f28741c0bf90451e3
takuyan89 <git.takuya89@gmail.com>
2024-10-09 14:06:48 +0900

    practice

--- my-app/src/app/(main)/pr/page.tsx
+++ my-app/src/app/(main)/pr/page.tsx
@@ -1,5 +1,20 @@
 const PrPage = () => {
-  return <div>PrPage</div>;
+  return (
+    <div className="flex justify-between items-center">
+      <div>test</div>
+      <div className="flex gap-10">
+        <div className="bg-slate-100">test2</div>
+        <div className="bg-slate-300">test3</div>
+        <div className="bg-slate-700">test4</div>
+      </div>
+      <h1 className="text-4xl text-green-700 text-center font-semibold">
+        hello
+      </h1>
+      <button className="bg-indigo-700 font-semibold text-white py-2 px-4 rounded transform hover:scale-110 hover:bg-red-700 duration-1000">
+        button
+      </button>
+    </div>
+  );
 };
 
 export default PrPage;
